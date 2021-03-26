09c2034014f255cd27718a1a2a843c98fe3e11af
VIGNESH J <vignesh@programmer.net>
2021-03-26 15:16:27 +0530

    Day 15
    After rebase

---
+++ Day15/src/ga/veee/day15/Dirlistonly.java
@@ -0,0 +1,29 @@
+package ga.veee.day15;
+
+import java.io.File;
+import java.io.FilenameFilter;
+
+class Onlyext implements FilenameFilter {
+    String ext;
+
+    public Onlyext(String e) {
+        ext = "." + e;
+    }
+
+    public boolean accept(File dir, String name) {
+        return name.endsWith(ext);
+    }
+}
+
+class Dirlistonly {
+    public static void main(String args[]) {
+        // change in to package name
+        String dirname = "./src/ga/veee/day15";
+        File f1 = new File(dirname);
+        FilenameFilter only = new Onlyext("java");
+        String s[] = f1.list(only);
+        for (int i = 0; i < s.length; i++) {
+            System.out.println(s[i]);
+        }
+    }
+}
